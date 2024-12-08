f312c3c0f8099863a4b8f91d784e0330fefb6e5a
un01s <96022876+un01s@users.noreply.github.com>
2024-12-08 14:10:04 -0700

    update

--- code/hello/src/main.rs
+++ code/hello/src/main.rs
@@ -39,11 +39,13 @@ impl Rectangle {
     let Point { x: x2, y: y2 } = self.p2;
     ((x1-x2)*(y1-y2)).abs()
   }
+
   fn perimeter(&self) -> f64 {
     let Point { x: x1, y: y1 } = self.p1;
     let Point { x: x2, y: y2 } = self.p2;
     2.0*((x1-x2).abs() + (y1-y2).abs())
   }
+
   fn translate(&mut self, x:f64, y:f64) {
     self.p1.x += x;
     self.p2.x += x;
