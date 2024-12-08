9a2e92850039b147d5aa58b8b2c6390d243552af
un01s <96022876+un01s@users.noreply.github.com>
2024-12-08 13:58:04 -0700

    update

--- code/hello/src/main.rs
+++ code/hello/src/main.rs
@@ -1,3 +1,4 @@
+// function to call function
 fn add_one(x: i32) -> i32 {
   x + 1
 }
@@ -11,6 +12,44 @@ fn hi_5<F>(func: F) where F: Fn(u32) -> u32, {
 }
 fn square(x: u32) -> u32 {
     x*x
+}
+
+struct Point {
+  x: f64,
+  y: f64,
+}
+
+impl Point {
+  fn origin() -> Point {
+    Point{x: 0.0, y: 0.0 }
+  }
+  fn new(x:f64, y:f64) -> Point {
+    Point{x: x, y: y}
+  }
+}
+
+struct Rectangle {
+  p1: Point,
+  p2: Point,
+}
+
+impl Rectangle {
+  fn area(&self) -> f64 {
+    let Point { x: x1, y: y1 } = self.p1;
+    let Point { x: x2, y: y2 } = self.p2;
+    ((x1-x2)*(y1-y2)).abs()
+  }
+  fn perimeter(&self) -> f64 {
+    let Point { x: x1, y: y1 } = self.p1;
+    let Point { x: x2, y: y2 } = self.p2;
+    2.0*((x1-x2).abs() + (y1-y2).abs())
+  }
+  fn translate(&mut self, x:f64, y:f64) {
+    self.p1.x += x;
+    self.p2.x += x;
+    self.p1.y += y;
+    self.p2.y += y;
+  }
 }
 
 fn main() {
@@ -30,4 +69,13 @@ fn main() {
   println!("closure_annotated: {}", closure_annotated(1));
   println!("closure_inferred: {}", closure_inferred(1));
   println!("closure returning one: {}", one());
+  
+  // methods
+  let rectangle = Rectangle {
+    // Associated functions are called using double colons
+    p1: Point::origin(),
+    p2: Point::new(3.0, 4.0),
+  };
+  println!("Rectangle perimeter: {}", rectangle.perimeter());
+  println!("Rectangle area: {}", rectangle.area());
 }
